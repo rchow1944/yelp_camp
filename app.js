d2f4f0a4e9d1e31fef84c680324a316962234825
rchow1944 <rchow2@buffalo.edu>
2017-09-28 07:53:24 +0000

    update db url

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var campgroundRoutes = require("./routes/campgrounds.js"),
     commentRoutes = require("./routes/comments.js"),
     indexRoutes = require("./routes/index.js");
 
+mongoose.connect(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost/yelp_camp_v13_deployed", {useMongoClient: true});
-mongoose.connect("mongodb://rchow:yelpcamp24@ds155684.mlab.com:55684/yelpcamp");
+//mongoose.connect("mongodb://rchow:yelpcamp24@ds155684.mlab.com:55684/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
